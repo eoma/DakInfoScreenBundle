df1ab8ec6e4a275cdb97d61da9afe740ba121f35
Endre Oma <eoma@kvarteret.no>
2011-09-04 16:41:38 +0200

    Configure the service dak_info_screen.module_manager

--- DependencyInjection/DakInfoScreenExtension.php
+++ DependencyInjection/DakInfoScreenExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\Definition\Processor;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -19,10 +20,13 @@ class DakInfoScreenExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $container->setParameter('dak_info_screen.modules', $config['modules']);
     }
 }
