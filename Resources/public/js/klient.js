8bc52c25ee7c1ad1bca14fad99cd41d8d9a629ee
Endre Oma <eoma@kvarteret.no>
2011-11-22 12:09:12 +0100

    Reformat dakSlideshowEditor to be of a Revealing Module Pattern

--- Resources/public/js/klient.js
+++ Resources/public/js/klient.js
@@ -1,250 +1,346 @@
 
+/**
+ * This object is responsible for initialising
+ * and maintaining the slideshow editing process.
+ *
+ * Singleton, revealing module pattern.
+ */
 var dakSlideshowEditor = function () {
-	var _slides;
+	var slides;
 
-	var _contentContainer;
+	var contentContainer;
+	var buttonList;
 
-	var _defaultData = {
+	var defaultData = {
 		defaultSlideDuration: 5000,
 		slides: [],
 	};
 
-	var _modules = [];
+	var modules = [];
 
-	var _form = null;
+	var sortStartPosition = null;
+	var sortStopPosition = null;
+
+	var form = null;
 
 	var config = {
 		form : "form",
 		content : "content",
 	};
 
-	function clickMinimiseMaximise (event) {
-		var elem = $(event.target);
 
-		if (elem.hasClass('toggled')) {
-			elem.text('Minimise');
-		} else {
-			elem.text('Maximise');
+	var addButton = function (name, callback) {
+		var button = $('<button />')
+			.attr('type', 'button')
+			.text(name);
+
+		if (jQuery.isFunction(callback)) {
+			button.click(callback);
 		}
 
-		elem.toggleClass('toggled');
-		elem.closest('#contentContainer > li').find('.canvas').toggle();
+		return button;
 	}
 
-	function clickMinimiseMaximiseAll (event) {
-		var elem = $(event.target);
+	var clickMinimiseMaximise = function (event) {
+		var button = $(event.target);
 
-		if (elem.hasClass('toggled')) {
-			elem.text('Minimise all');
-			_contentContainer.find('.toolbar .minimise.toggled').click();
+		if (button.hasClass('toggled')) {
+			button.text('Minimise');
 		} else {
-			elem.text('Maximise all');
-			_contentContainer.find('.toolbar .minimise').not('.toggled').click();
+			button.text('Maximise');
 		}
 
-		elem.toggleClass('toggled');
+		button.toggleClass('toggled');
+		button.closest('#contentContainer > li').find('.canvas').toggle();
 	}
 
-	function deleteSlide (event) {
+	var clickMinimiseMaximiseAll = function (event) {
+		var button = $(event.target);
+
+		if (button.hasClass('toggled')) {
+			button.text('Minimise all');
+			contentContainer.find('.toolbar .minimise.toggled').click();
+		} else {
+			button.text('Maximise all');
+			contentContainer.find('.toolbar .minimise').not('.toggled').click();
+		}
+
+		button.toggleClass('toggled');
+	}
+
+	var deleteSlide = function (event) {
 		var ancestor = $(event.target).closest('#contentContainer > li');
 
 		var slideIndex = ancestor.index();
-		_slides[slideIndex].module = null;
+		slides[slideIndex].module = null;
 
-		//delete _slides[slideIndex];
+		//delete slides[slideIndex];
 
-		_slides.splice(slideIndex, 1);
+		slides.splice(slideIndex, 1);
 
 		ancestor.empty().remove();
 	}
-	
-	function addRawSlide () {
-		var index = _slides.length;
 
-		_slides[index] = {
-			content : "<section>\n\n</section>",
+	var submit = function (e) {
+		if ($('#doNotUpdate').is('checked')) {
+			e.preventDefault();
+		}
+		var newSlides = defaultData;
+
+		console.log(slides);
+
+		newSlides.defaultSlideDuration = parseInt(jQuery('#defaultSlideDuration').val(), 10);
+
+		for (var i = 0; i < slides.length; i++) {
+			newSlides.slides[i] = {
+				content : "",
+				css : ""
+			};
+
+			newSlides.slides[i].content = slides[i].module.getRawContent();
+			newSlides.slides[i].css = slides[i].module.getRawCss();
+		}
+
+		var jsonSlide = JSON.stringify(slides);
+		console.log(jsonSlide);
+
+		form.find('#' + config.content).val(jsonSlide);
+	};
+
+	var getSlideTemplate = function() {
+		var domainElem = $("<li />")
+			.append($("<span />")
+				.addClass("title")
+			)
+			.append($("<div />")
+				.addClass("toolbar cf")
+				.append($("<ul />")
+					.addClass("left")
+					.append($("<li />")
+						.append(
+							addButton("Minimise").addClass("minimise")
+						)
+					)
+				)
+				.append($("<ul />")
+					.addClass("right")
+
+					.append($("<li />")
+						.append(
+							addButton("Delete").addClass("delete")
+						)
+					)
+				)
+			)
+			.append($("<div>")
+				.addClass("canvas")
+			)
+		;
+
+		return domainElem;
+	};
+
+	var addSlideTemplate = function () {
+		var domainElem = getSlideTemplate();
+
+		contentContainer.append(domainElem);
+
+		return domainElem;
+	};
+
+	var identifySlide = function(slideIndex, domainElem) {
+		var slide = slides[slideIndex];
+
+		for (var i = 0; i < modules.length; i++) {
+			var m = modules[i];
+			//console.log("identifySlide loop", _modules[i]);
+			if (jQuery.isFunction(m.identifySlide) && m.identifySlide(slide)) {
+				slide.module = m(slide, domainElem);
+			}
+		}
+
+		if (!("module" in slide)) {
+			slide.module = new dakSlideShowRawEditor(slide, domainElem);
+		}
+
+		slides[slideIndex] = slide;
+	};
+
+	var addSlide = function (callback) {
+		var index = slides.length;
+
+		slides[index] = {
+			content : "",
 			css : "",
 		};
 
-		_slides[index].module = dakSlideShowRawEditor(_slides[index], t.addSlide());
-	}
+		slides[index].module = callback(slides[index], addSlideTemplate());
+	};
+
+	/**
+	 * Registers a slideshow editor module
+	 * functionObject should be a function that has
+	 * publicly accessible, static methods identifySlide() (optional, recommended)
+	 * and staticButtonSetup() (optional, recommended)
+	 */
+	var registerModule = function(functionObject) {
+		modules.push(functionObject);
+	};
+
+	var init = function(data, customConfig) {
+		jQuery.extend(config, customConfig);
+
+		console.log(config);
+
+		form = jQuery('#' + config.form);
+		contentContainer = jQuery('#contentContainer');
+		buttonList = jQuery('#newSlideButtons');
+
+		function addSlideButton (name, callback) {
+			var button = addButton(
+				name,
+				function () {
+					addSlide(callback);
+				}
+			);
+
+			return button;
+		}
+
+		if (modules.length > 0) {
+
+			for (var i = 0; i < modules.length; i++) {
+				var m = modules[i];
+
+				if (jQuery.isFunction(m.staticButtonSetup)) {
+					var button = m.staticButtonSetup();
+
+					console.log("button", button);
+
+					if (jQuery.isArray(button)) {
+
+					} else {
+
+						buttonList.append($('<li />')
+							.append(
+								addSlideButton(button.name, button.callback)
+							)
+						);
+					}
+
+					button = null;
+				}
+
+				m = null;
+			}
+		}
+
+		if (jQuery.isArray(data.slides)) {
+			slides = data.slides;
+		} else {
+			slides = [];
+		}
+
+		form.submit(submit);
+
+		jQuery('#defaultSlideDuration').val(data.defaultSlideDuration);
+
+		jQuery('#contentContainer').sortable({
+			placeholder : "ui-state-highlight",
+				
+			start : function (event, ui) {
+				var domainElem = ui.item;
+				sortStartPosition = domainElem.index();
+			},
+				
+			update : function (event, ui) {
+				var domainElem = ui.item;
+				sortStopPosition = domainElem.index();
+					
+				var diff = sortStopPosition - sortStartPosition;
+
+				var tmp = slides[sortStartPosition];
+
+				if (diff > 0) {
+					for (var i = sortStartPosition; i < sortStopPosition; i++) {
+						slides[i] = slides[i+1];
+					}
+				} else {
+					for (var i = sortStartPosition; i > sortStopPosition; i--) {
+						slides[i] = slides[i-1];
+					}
+				}
+				slides[sortStopPosition] = tmp;
+					
+				sortStartPosition = null;
+				sortStopPosition = null;
+			},
+
+			delay : 200,
+		});
+
+		contentContainer.find('.toolbar .delete').live('click', deleteSlide);
+		contentContainer.find('.toolbar .minimise').live('click', clickMinimiseMaximise);
+
+		jQuery('#minimiseMaximiseAll').live('click', clickMinimiseMaximiseAll);
+
+		for (var i = 0; i < slides.length; i++) {
+			var slideTemplate = addSlideTemplate();
+
+			identifySlide(i, slideTemplate);
+		}
+	};
 
 	return {
-		t : null,
-		sortStartPosition : null,
-		sortStopPosition : null,
+		init : init,
+		//submit : submit,
+		//getSlideTemplate : getSlideTemplate,
+		//addSlideTemplate : addSlideTemplate,
 
-		init : function (data, customConfig) {
-			t = this;
+		//identifySlide : identifySlide,
 
-			jQuery.extend(config, customConfig);
+		registerModule : registerModule,
 
-			console.log(config);
-
-			_form = jQuery('#' + config.form);
-			_contentContainer = jQuery('#contentContainer');
-
-			_modules = [];
-
-			if (jQuery.isArray(data.slides)) {
-				_slides = data.slides;
-			} else {
-				_slides = [];
-			}
-
-			_form.submit(t.submit);
-
-			jQuery('#defaultSlideDuration').val(data.defaultSlideDuration);
-
-			jQuery('#contentContainer').sortable({
-				placeholder : "ui-state-highlight",
-				
-				start : function (event, ui) {
-					var elem = ui.item;
-					t.sortStartPosition = elem.index();
-				},
-				
-				update : function (event, ui) {
-					var elem = ui.item;
-					t.sortStopPosition = elem.index();
-					
-					var diff = t.sortStopPosition - t.sortStartPosition;
-
-					var tmp = _slides[t.sortStartPosition];
-
-					if (diff > 0) {
-						for (var i = t.sortStartPosition; i < t.sortStopPosition; i++) {
-							_slides[i] = _slides[i+1];
-						}
-					} else {
-						for (var i = t.sortStartPosition; i > t.sortStopPosition; i--) {
-							_slides[i] = _slides[i-1];
-						}
-					}
-					_slides[t.sortStopPosition] = tmp;
-					
-					t.sortStartPosition = null;
-					t.sortStopPosition = null;
-				},
-
-				delay : 200,
-			});
-
-			jQuery('#addSlide').click(addRawSlide);
-
-			_contentContainer.find('.toolbar .delete').live('click', deleteSlide);
-			_contentContainer.find('.toolbar .minimise').live('click', clickMinimiseMaximise);
-
-			jQuery('#minimiseMaximiseAll').live('click', clickMinimiseMaximiseAll);
-
-			for (var i = 0; i < _slides.length; i++) {
-				var elem = t.addSlide();
-
-				t.identifySlide(i, elem);
-			}
-		},
-
-		submit : function (e) {
-			//e.preventDefault();
-			var slides = _defaultData;
-
-			console.log(_slides);
-
-			slides.defaultSlideDuration = parseInt(jQuery('#defaultSlideDuration').val(), 10);
-
-			for (var i = 0; i < _slides.length; i++) {
-				slides.slides[i] = {
-					content : "",
-					css : ""
-				};
-
-				slides.slides[i].content = _slides[i].module.getRawContent();
-				slides.slides[i].css = _slides[i].module.getRawCss();
-			}
-
-			var jsonSlide = JSON.stringify(slides);
-			console.log(jsonSlide);
-
-			_form.find('#' + config.content).val(jsonSlide);
-		},
-
-		getSlideTemplate : function () {
-			var elem = $("<li />")
-				.append($("<span />")
-					.addClass("title")
-				)
-				.append($("<div />")
-					.addClass("toolbar cf")
-					.append($("<ul />")
-						.addClass("left")
-						.append($("<li />")
-							.addClass("minimise")
-							.text("Minimise")
-						)
-					)
-					.append($("<ul />")
-						.addClass("right")
-
-						.append($("<li />")
-							.addClass("delete")
-							.text("Delete")
-						)
-					)
-				)
-				.append($("<div>")
-					.addClass("canvas")
-				)
-			;
-
-			return elem;
-		},
-
-		addSlide : function () {
-			var elem = t.getSlideTemplate();
-
-			_contentContainer.append(elem);
-
-			return elem;
-		},
-
-		identifySlide : function (slideIndex, elem) {
-			for (var i = 0; i < _modules.length; i++) {
-				if (_modules[i].identifySlide(_slides[slideIndex])) {
-					_slides[slideIndex].module = _modules[i](_slides[slideIndex], elem);
-				}
-			}
-
-			if (!("module" in _slides[slideIndex])) {
-				_slides[slideIndex].module = new dakSlideShowRawEditor(_slides[slideIndex], elem);
-			}
-		},
-
-		registerModule : function (instantiatorFunction) {
-			_modules.append(instantiatorFunction);
-		}
+		//addSlide : addSlide,
+		addButton : addButton
 	};
 
 }();
 
 var dakSlideShowRawEditor = function (slide, elem) {
 
-	var _elem = elem;
-	_elem.find('.title').eq(0).text('Raw slide');
-	var _canvas = _elem.find('.canvas').eq(0);
+	if (slide.content == "") {
+		slide.content = "<section>\n\n</section>";
+	}
+
+	elem.find('.title').eq(0).text('Raw slide');
+	var canvas = elem.find('.canvas').eq(0);
 	
 	var textareaHtml = $('<textarea class="html" rows="10" cols="50">').val(slide.content);
 	var textareaCss = $('<textarea class="css" rows="10" cols="50">').val(slide.css);
 
-	_canvas.append(textareaHtml);
-	_canvas.append(textareaCss);
+	canvas.append(textareaHtml);
+	canvas.append(textareaCss);
+
+	var getRawContent = function () {
+			return canvas.find('textarea.html').val();
+	}
+
+	var getRawCss = function () {
+			return canvas.find('textarea.css').val();
+	}
 
 	return {
-		getRawContent : function () {
-			return _canvas.find('textarea.html').val();
-		},
-
-		getRawCss : function () {
-			return _canvas.find('textarea.css').val();
-		}
+		getRawContent : getRawContent,
+		getRawCss : getRawCss
 	}
 };
+
+dakSlideShowRawEditor.staticButtonSetup = function () {
+	return {
+		name : "Add raw slide",
+		callback : dakSlideShowRawEditor,
+	};
+}
+
+dakSlideshowEditor.registerModule(dakSlideShowRawEditor);
